49da8cdc1e40926407c7adae51096ce255e63deb
monsur776e <monsuralam8897@gmail.com>
2023-05-16 16:34:26 +0600

    data load from server success

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { useLoaderData } from "react-router-dom";
+
 function App() {
+   const coffes = useLoaderData();
+   console.log(coffes);
    return (
       <>
          <h2 className="text-6xl text-purple-600">Hello yeaaaaaaaaaah world</h2>
